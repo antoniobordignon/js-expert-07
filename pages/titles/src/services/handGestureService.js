cac15a2b18be417060bd757a281f0308ff051546
Antonio <antoniovictorbordignon@outlook.com>
2023-02-28 15:10:57 -0300

    feat: configure the detector and understand

---
+++ pages/titles/src/services/handGestureService.js
@@ -0,0 +1,28 @@
+export default class HandGestureService {
+    #fingerpose 
+    #handPoseDetection
+    #handsVersion
+    #detector = null
+    
+    constructor({ fingerpose, handPoseDetection, handsVersion }) {
+        this.fingerpose = fingerpose
+        this.handPoseDetection = handPoseDetection 
+        this.handsVersion =handsVersion 
+    }
+
+    async initializeDetector() {
+        if(this.#detector) return this.#detector
+
+        const detectorConfig = {
+            runtime: 'mediapipe',
+            solutionPath: `https://cdn.jsdelivr.net/npm/@mediapipe/hands@${this.#handsVersion}`,
+            modelType: 'lite',
+            maxHands: 2,
+        }
+        this.#detector = await this.handPoseDetection.createDetector(
+            this.#handPoseDetection.SupportedModels.MediaPipeHands, 
+            detectorConfig
+        )
+        return this.#detector
+    }
+}
